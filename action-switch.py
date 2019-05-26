c306fcf38eade8bc52fab4e4312e4e84641467dd
respeaker <respeaker@seeed.cc>
2019-05-26 23:28:15 +0200

    script commit

--- action-switch.py
+++ action-switch.py
@@ -8,7 +8,7 @@ from hermes_python.ontology import *
 import io
 import requests
 import json
-# import jellyfish
+import jellyfish
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
