0541a6c8f91343b6a84daa863f98d5e9d8dfe456
respeaker <respeaker@seeed.cc>
2019-05-26 22:57:33 +0200

    script commit

--- action-switch.py
+++ action-switch.py
@@ -39,8 +39,8 @@ def getSceneNames(conf,myListSceneOrSwitch):
     for scene in jsonresponse["result"]:
         myName=scene["Name"].encode('utf-8')
         myListSceneOrSwitch[(scene["idx"])] = {'Type':'switchscene','Name':myName}
+    print('---------SceneName----------')
     return myListSceneOrSwitch
-
 
 def getSwitchNames(conf,myListSceneOrSwitch):
     myURL="http://"+conf.get("secret").get("hostname")+':'+conf.get("secret").get("port")+'/json.htm?type=command&param=getlightswitches'
@@ -49,40 +49,38 @@ def getSwitchNames(conf,myListSceneOrSwitch):
     for sw in jsonresponse["result"]:
         myName=sw["Name"].encode('utf-8')
         myListSceneOrSwitch[(sw["idx"])] = {'Type':'switchlight','Name':myName}
+    print('---------SwitchName----------')
     return myListSceneOrSwitch
-
 
 def BuildActionSlotList(intent):
     intentSwitchList=list()
     intentSwitchActionList=list()
     intentSwitchState='None' #by default if no action
     for (slot_value, slot) in intent.slots.items():
-        print(slot_value)
+        print("Slot_value: "+slot_value)
         if slot_value=="action" or slot_value=="switch":
             for slot_value2 in slot.all():
-              print(slot_value2.value)
-
+              print("Slot_Value2: "+slot_value2.value)
     print("---------------------------------")
     for (slot_value, slot) in intent.slots.items():
-        print(slot_value)
+        print("Slot_value: "+slot_value)
         if slot_value=="action":
             #NLU parsing does not preserve order of slot, thus it is impossible to have different action ON and OFF in the same intent=> keep only the first:
             if slot[0].slot_value.value.value=="TurnOn":
                 intentSwitchState='On'
             else :
-                intentSwitchState='Off'   
-            print(intentSwitchState)
+                intentSwitchState='Off'
+            print("SchalterStatus: "+intentSwitchState)
         elif slot_value=="switch":
             for slot_value2 in slot.all():
                 intentSwitchList.append(slot_value2.value)
-                print(slot_value2.value)
+                print("Slotvalue: "+slot_value2.value)
 
     if not intentSwitchState=='None':
         for mySwitch in intentSwitchList:
             intentSwitchActionList.append({'Name':mySwitch,'State':intentSwitchState})
             print(mySwitch+"------>"+intentSwitchState)
     return intentSwitchActionList
-
 
 def curlCmd(idx,myCmd,myParam,conf):
     command_url="http://"+conf.get("secret").get("hostname")+':'+conf.get("secret").get("port")+'/json.htm?type=command&param='+myParam+'&idx='+str(idx)+'&switchcmd='+myCmd
@@ -98,7 +96,7 @@ def ActionneEntity(name,action,myListSceneOrSwitch,conf):
     DomoticzRealName=""
     print("ActionneEntity: "+MyWord)
     for idx,scene in myListSceneOrSwitch.items():
-#        print(str(scene['Name'],'utf-8'))
+        print(str(scene['Name'],'utf-8'))
         distance = 1-jellyfish.jaro_distance(str(scene['Name'],'utf-8'), MyWord)
     #    print "Distance is "+str(distance)
         if distance < lowest_distance:
@@ -114,7 +112,7 @@ def ActionneEntity(name,action,myListSceneOrSwitch,conf):
         #print(lowest_idx)
         curlCmd(lowest_idx,action,lowest_Type,conf)
         return True,DomoticzRealName
-        #hermes.publish_end_session(intent_message.session_id, "j'allume "+lowest_name)
+        hermes.publish_end_session(intent_message.session_id, "Einschalten "+lowest_name)
     else:
         return False,DomoticzRealName
 
@@ -140,8 +138,7 @@ def action_wrapperOrdreDirect(hermes, intentMessage, conf):
     DomoticzRealName=""
     MyAction=ActionneEntity(actionText,'On',myListSceneOrSwitch,conf)
     result_sentence = u"OK. Du hast {} angefordert.".format(str(MyAction[1]))  # The response that will be said out loud by the TTS engine.
-
-    if MyAction[0] : 
+    if MyAction[0] :
         hermes.publish_end_session(intentMessage.session_id, result_sentence)
     else:
         print("pas d action")
@@ -151,20 +148,23 @@ def action_wrapperOrdreDirect(hermes, intentMessage, conf):
 def action_wrapperOrdre(hermes, intentMessage, conf):
     myListSceneOrSwitch=dict()
     myListSceneOrSwitch= getSceneNames(conf,myListSceneOrSwitch)
+    print("Scenen ermitteln")
     myListSceneOrSwitch= getSwitchNames(conf,myListSceneOrSwitch)
+    print("Schalter ermitteln")
     intentSwitchActionList=BuildActionSlotList(intentMessage)
     actionText=""
     myAction = True
     for intentSwitchAction in intentSwitchActionList:
+        print("intentSwitchAction")
         Match= ActionneEntity(intentSwitchAction["Name"],intentSwitchAction["State"],myListSceneOrSwitch,conf)
         DomoticzRealName=Match[1]
         myAction=myAction and Match[0]
-        if intentSwitchAction["State"]=="On": 
+        if intentSwitchAction["State"]=="On":
             texte = u"Einschalten"
         else:
             texte = u"Ausschalten"
         actionText=u'{}, {} {}'.format(actionText,texte,str(DomoticzRealName))
-    if myAction and len(intentSwitchActionList)>0: 
+    if myAction and len(intentSwitchActionList)>0:
         hermes.publish_end_session(intentMessage.session_id, actionText)
     else:
         hermes.publish_end_session(intentMessage.session_id, u"Entschuldigung, ich habe es nicht verstanden.")
